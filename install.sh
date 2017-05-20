2bc351447e0a0c471084e6c625b32aa6f2cf1162
JING DONG <me@jing.io>
2017-05-20 22:05:24 +0100

    add installation information

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+# install vim-plug as plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
+
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.local.conf
